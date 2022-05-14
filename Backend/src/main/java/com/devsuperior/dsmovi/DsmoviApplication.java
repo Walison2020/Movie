66351053788cdea37636bff6827fb43458a37639
Your Name <youremail@domain.com>
2022-05-14 16:56:05 -0300

    Projeto Criado

---
+++ Backend/src/main/java/com/devsuperior/dsmovi/DsmoviApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoviApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/devsuperior/dsmovi/DsmoviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
