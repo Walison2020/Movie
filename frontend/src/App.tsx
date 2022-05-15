4582f99fe31d4b543ff26d396053a424c7e28119
Your Name <youremail@domain.com>
2022-05-15 19:21:04 -0300

    Rout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
-import Navbar from "./Componentes/Navbar";
-
-
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/listing';
+import Form from 'pages/Forms';
+import Navbar from "Componentes/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
